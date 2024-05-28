799c3a8702751906a2d7634c95f2f0db04dc1a25
TarfeehaHussain <164815147+Tarfeehaww@users.noreply.github.com>
2024-05-29 01:19:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const ans = await inquirer.prompt([{
+        name: "sent",
+        type: "input",
+        message: "Enter your sentence to count the given words:"
+    }]);
+//    output defines
+const Done = ans.sent.trim().split(" ");
+console.log(Done);
+console.log(`Your sentence word count is ${Done.length}`);
